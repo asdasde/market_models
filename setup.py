2d9c1f091e5a717d81371b3847b9a7168162b1f7
asdasde <erceg.luka@jjzmaj.edu.rs>
2023-11-14 20:32:12 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Modeling competitor quotes (primary Casco) using xgboost and quantile regression.',
+    author='Luka Erceg',
+    license='',
+)
